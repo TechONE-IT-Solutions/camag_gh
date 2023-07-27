0f3da5c2c747c326d724cc5308b05036ad18603c
yhawbee1 <antwibrian82@gmail.com>
2023-07-27 14:21:10 +0000

    Success page Update

--- resources/views/Admin/about.blade.php
+++ resources/views/Admin/about.blade.php
@@ -3,29 +3,29 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-5">
-        <form class="form-control p-5">
-                <label for="aboutImage">About Image</label>
-                <input type="file" name="aboutImage" id="aboutImage" class="form-control">
-                <button type="button" class="btn btn-primary px-4 m-3">Add</button>
+    <div class="row">
+        <div class="col-5">
+            <form class="form-control p-5">
+                    <label for="aboutImage">About Image</label>
+                    <input type="file" name="aboutImage" id="aboutImage" class="form-control">
+                    <button type="button" class="btn btn-primary px-4 m-3">Add</button>
             </form>
         </div>
-    <div class="col 7">
-        <div class="row">
-            <div class="col-6">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="{{asset('CAMAGADMIN/assets/img/product-1.jpg') }}" class="img-fluid" alt="">
-                    </div>
-                    <div class="card-body pt-3">
-                        <button type="button" class="btn btn-primary">Delete</button>
+        <div class="col 7">
+            <div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="{{asset('CAMAGADMIN/assets/img/product-1.jpg') }}" class="img-fluid" alt="">
+                        </div>
+                        <div class="card-body pt-3">
+                            <button type="button" class="btn btn-primary">Delete</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 @endsection
