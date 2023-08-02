dc77dfbbbdeb1b2e928b10122ab819e4fc83199a
hakim <hakim@gmail.com>
2023-08-02 08:53:03 +0000

    yes

--- app/Http/Controllers/website/registerController.php
+++ app/Http/Controllers/website/registerController.php
@@ -37,6 +37,19 @@ class registerController extends Controller
         $myimage = time() . '.' . $request->image->getClientOriginalName();
         $request->image->move(public_path($destinationPath),$myimage);
 
+        switch($request->input('pos')){
+            case('AssemblyMember'):
+                $membershipid = 'ASM-'.random_int(10000, 99999);
+                Break;
+            case('UnitCommiteeMember'):
+                $membershipid = 'UCM-'.random_int(10000, 99999);
+                Break;
+            case('AssociateMember'):
+                $membershipid = 'ASS-'.random_int(10000, 99999);
+                Break;
+        }
+
+
         $postData = [
             'name' => $request->input('name'),
             'hometown' => $request->input('hometown'),
@@ -59,10 +72,13 @@ class registerController extends Controller
             'emergency_relation' => $request->input('emergency_relation'),
             'emergency_contact' => $request->input('emergency_contact'),
             'signature' => $request->input('signature'),
+            'membership_id'=> $membershipid
+
+
         ];
         homepage::create($postData);
         return redirect('/success')->with(['message' => 'Registration successful!!', 'status'=> 'success']);
-    
+
     }
 
     /**
