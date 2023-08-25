baf9bf3a95911113f92ab68b0b9e9497255e19c8
hakim <hakim@gmail.com>
2023-08-25 11:02:58 +0000

    maintained

--- resources/views/website/gallery.blade.php
+++ resources/views/website/gallery.blade.php
@@ -26,51 +26,22 @@
                 <h3 class=" mb-5">Photos Of Our Current Programs</h3>
             </div>
             <div class="owl-carousel project-carousel wow fadeInUp" data-wow-delay="0.1s">
-                <div class="project-item mb-5">
-                    <div class="position-relative bg-light">
-                        <img class="img-fluid" src="{{asset('CAMAG/img/project-2.jpg') }}" alt="">
-                        <div class="project-overlay">
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="{{asset('CAMAG/img/project-2.jpg') }}"
-                                data-lightbox="project"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href=""><i
-                                    class="fa fa-link"></i></a>
+                @foreach ($Gallery as $picture)
+                    <div class="project-item mb-5">
+                        <div class="position-relative bg-light">
+                            <img class="img-fluid" src="{{asset($picture['image']) }}" alt="">
+                            <div class="project-overlay">
+                                <a class="btn btn-lg-square btn-light rounded-circle m-1" href="{{asset('CAMAG/img/project-2.jpg') }}"
+                                    data-lightbox="project"><i class="fa fa-eye"></i></a>
+                                <a class="btn btn-lg-square btn-light rounded-circle m-1" href=""><i
+                                        class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="p-4">
+                            <span>{{ $picture['text']}}</span>
                         </div>
                     </div>
-                    <div class="p-4">
-                        <a class="d-block h5" href="">Marketing Content Strategy</a>
-                        <span>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem</span>
-                    </div>
-                </div>
-                <div class="project-item mb-5">
-                    <div class="position-relative">
-                        <img class="img-fluid" src="{{asset('CAMAG/img/project-3.jpg') }}" alt="">
-                        <div class="project-overlay">
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="{{asset('CAMAG/img/project-3.jpg') }}"
-                                data-lightbox="project"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href=""><i
-                                    class="fa fa-link"></i></a>
-                        </div>
-                    </div>
-                    <div class="p-4">
-                        <a class="d-block h5" href="">Business Target Market</a>
-                        <span>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem</span>
-                    </div>
-                </div>
-                <div class="project-item mb-5">
-                    <div class="position-relative">
-                        <img class="img-fluid" src="{{asset('CAMAG/img/project-4.jpg') }}" alt="">
-                        <div class="project-overlay">
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href="{{asset('CAMAG/img/project-4.jpg') }}"
-                                data-lightbox="project"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-lg-square btn-light rounded-circle m-1" href=""><i
-                                    class="fa fa-link"></i></a>
-                        </div>
-                    </div>
-                    <div class="p-4">
-                        <a class="d-block h5" href="">Social Marketing Strategy</a>
-                        <span>Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem</span>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
